17ca6d3aae82fd96e0ab82fa9df9cb007f9b822c
diegosagreda <diegoagreda1996@gmail.com>
2025-01-26 11:09:43 -0500

    start project

---
+++ helloWord.go
@@ -0,0 +1,35 @@
+package main
+import "fmt"
+func main()  {
+	fmt.Println("Hola mundo")
+
+	//Tipos de datos-----------------------
+	// var nombre string = "Diego"
+	// var apellido = "Agreda"
+	// nombre1 := "Jamioy"
+
+	// fmt.Println(nombre + apellido + nombre1)
+
+	// //Arrays ------------------------------
+	// //var fruits = [4]string{"Manzana", "Pera"}
+	// //fruits1 := [4]string{"Manzana", "Pera"}
+
+	// //Arreglos dinamicos
+	// var nombres = []string{}
+
+	// nombres = append(nombres,"Diego")
+
+	// fmt.Println(nombres)
+
+	// //Mapas
+	// var paises = map[string]string{
+	// 	"Colombia":"Bogota",
+	// 	"Peru":"Lima",
+	// }
+	// fmt.Println(paises)
+	// fmt.Println("Capital de Colombia: ", paises["Colombia"])
+
+		
+
+
+}
